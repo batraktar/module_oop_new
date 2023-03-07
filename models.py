24e6b564165535ee685c1bf3e128d6f3902b7544
Batrak <maks.batrak1222@gmail.com>
2023-03-08 00:39:00 +0200

    finally...(it's false)

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 import random
-from settings import PLAYER_LIVES
+from settings import PLAYER_LIVES, ALLOWED_ATTACKS
 from game_exceptions import EnemyDown, GameOver
 
 
@@ -11,56 +11,61 @@ class Enemy:
     @staticmethod
     def select_attack():
         """ choice to attack the enemy """
-        return random.randint(1, 3)
+        return 1
 
     def decrease_lives(self):
         """ enemy lives """
         self.lives -= 1
         if self.lives == 0:
-            raise EnemyDown()
+            raise EnemyDown
 
 
 class Player:
-    def __init__(self, name):
+    def __init__(self, name, gmode):
         self.name = name
+        self.gmode = gmode
         self.score = 0
         self.lives = PLAYER_LIVES
-        self.allowed_attacks = [1, 2, 3]
+        self.allowed_attacks = ALLOWED_ATTACKS
 
     @staticmethod
     def fight(attack, defence):
         """ method w"""
         if attack == defence:
             return 0
-        if (attack == 1 and defence == 2) or\
-                (attack == 2 and defence == 3) or\
-                (attack == 3 and defence == 1):
+        if attack == 1 and defence == 2:
+            return 1
+        if attack == 2 and defence == 3:
+            return 1
+        if attack == 3 and defence == 1:
             return 1
         return -1
 
     def decrease_lives(self):
         self.lives -= 1
         if self.lives == 0:
-            return GameOver("")
+            raise GameOver(self.score, self.name, self.gmode)
+        print(f'Your lives: {self.lives}')
 
-    def attack(self, enemy_obj):
+    def attack(self, enemy_obj, mode):
         """ choose player """
         while True:
             try:
                 attack = int(input(f"{self.name}, choose your attack(1, 2, 3):\n"))
-                if attack not in self.allowed_attacks:
+                if attack not in ALLOWED_ATTACKS:
                     raise ValueError("Invalid attack choice.")
                 break
-            except ValueError as enemy:
-                print(f"Invalid input. {enemy}")
+            except ValueError:
+                print(f"Invalid input.")
         defence = enemy_obj.select_attack()
-        result = Player.fight(attack, defence)
+        result = self.fight(attack, defence)
         if result == 0:
             print("It's draw!")
         elif result == 1:
             print("You attacked successfully!")
+            self.score += (1 * mode)
+            print(f'Your score: {self.score}')
             enemy_obj.decrease_lives()
-            self.score += 1
         elif result == -1:
             print("You missed!")
             
@@ -68,17 +73,17 @@ class Player:
         while True:
             try:
                 defence = int(input(f"{self.name}, choose your defence(1, 2, 3):\n"))
-                if defence not in self.allowed_attacks:
-                    raise ValueError("Invalid defence choice.")
+                if defence not in ALLOWED_ATTACKS:
+                    raise ValueError("Invalid defence choice. Please enter a number from 1-3.")
                 break
-            except ValueError as enemy:
-                print(f"Invalid input. {enemy}")
+            except ValueError:
+                print(f"Invalid input.")
         attack = enemy_obj.select_attack()
-        result = Player.fight(attack, defence)
+        result = self.fight(attack, defence)
         if result == 0:
             print("It's draw!")
         elif result == 1:
             print("You defence worked!")
-        else:
-            print("Enemy attack was successfully!")
+        elif result == -1:
+            print(f"Enemy attack was successfully! Your lives: {self.lives}")
             self.decrease_lives()
--- settings.py
+++ settings.py
@@ -1,1 +1,5 @@
 PLAYER_LIVES = 1
+ALLOWED_ATTACKS = [1, 2, 3]
+MENU = ['Start', 'Show scores', 'Exit', 'Help']
+
+HARD = 3
